20a988eafcdca35d2cc3df95f7af80ebb6ce1075
Álvaro Frutos Gutiérrez <alvarofrutos@yahoo.com>
2016-05-30 20:59:49 +0200

    Initial Commit

---
+++ IRPFCalculatorLibrary/IRPFCalculatorLibrary.h
@@ -0,0 +1,19 @@
+//
+//  IRPFCalculatorLibrary.h
+//  IRPFCalculatorLibrary
+//
+//  Created by Álvaro Frutos Gutiérrez on 30/5/16.
+//  Copyright © 2016 Álvaro Frutos Gutiérrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IRPFCalculatorLibrary.
+FOUNDATION_EXPORT double IRPFCalculatorLibraryVersionNumber;
+
+//! Project version string for IRPFCalculatorLibrary.
+FOUNDATION_EXPORT const unsigned char IRPFCalculatorLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRPFCalculatorLibrary/PublicHeader.h>
+
+
